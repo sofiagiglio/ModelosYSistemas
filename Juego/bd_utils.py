edeb7bb4a22f6657102e1bec97da9f3c4a020618
Abigail2006 <abalbuena@sagradoalcal.edu.ar>
2022-08-08 16:20:46 -0300

    8 de agosto: Modificamos un poco la interfaz gráfica, se vienen cositaas

--- Juego/bd_utils.py
+++ Juego/bd_utils.py
@@ -10,16 +10,18 @@ def traer_carta_random():
     pos = random.randint(0,len(ids)-1)
     #print(ids[pos][0])
     id_busqueda = ids[pos][0]
-    c.execute(f"SELECT URLFOTO, VIDA, DANIO FROM CARTAS WHERE ID = {id_busqueda} ")
+    c.execute(f"SELECT URLFOTO, VIDA, DANIO, VELOCIDAD, NOMBRE FROM CARTAS WHERE ID = {id_busqueda} ")
     resp = c.fetchall()
     url = resp[0][0]
     vida = resp[0][1]
     danio = resp[0][2]
+    velocidad = resp[0][3]
+    nombre = resp[0][4]
     lista = url.split("/")
     pre = 'https://www.deckshop.pro/img/card_ed/'
     post = '.png'
     url_foto = pre + lista[len(lista)-1] + post
-    return (url_foto, vida, danio)
+    return (url_foto, vida, danio, velocidad, nombre)
 
 
 def traer_url_foto_por_id(id_busqueda):
--- Juego/pruebitauwu88.py
+++ Juego/pruebitauwu88.py
@@ -30,28 +30,47 @@ f2 = tkinter.Frame(ventana)
 f2.grid(column=1, row=0)
 f3 = tkinter.Frame(ventana)
 f3.grid(column=2, row=0)
+f4 = tkinter.Frame(ventana)
+f4.grid(column=0, row=1)
 
 carta1 = preparar_carta()
 ph1 = carta1[0]
+vida1 = carta1[1]
+danio1 = carta1[2]
+velocidad1 = carta1[3]
+nombre1 = carta1[4]
+
+
+
 l1 = tkinter.Label(f1, image=ph1)
 l1.grid(column=0, row=0)
 
 carta2 = preparar_carta()
 ph2 = carta2[0]
+
+
+
+
 lomo = ImageTk.PhotoImage(Image.open('lomo_clash.png'))
 l2 = tkinter.Label(f3, image=lomo)
 l2.grid(column=0, row=0)
 
 boton1 = tkinter.Button(f2, text = "Vida", command = lambda: print("La vida de j1 es _ y la de j2 es _"))
 boton1.pack()
-
 boton2 = tkinter.Button(f2, text = "Daño", command = lambda: print("El daño de j1 es _ y la de j2 es _"))
 boton2.pack()
+boton3 = tkinter.Button(f2, text = "Velocidad", command = lambda: print("La velocidad de j1 es _ y la de j2 es _"))
+boton3.pack()
+boton4 = tkinter.Button(f2, text = "Rendirse", command = lambda: print("_ se rindió"))
+boton4.pack()
 
-boton2 = tkinter.Button(f2, text = "Velocidad", command = lambda: print("La velocidad de j1 es _ y la de j2 es _"))
-boton2.pack()
-
-boton2 = tkinter.Button(f2, text = "Rendirse", command = lambda: print("_ se rindió"))
-boton2.pack()
+etiqueta1 = tkinter.Label(f4, text = f'NOMBRE: {nombre1}')
+etiqueta1.pack()
+etiqueta2 = tkinter.Label(f4, text = f'DAÑO: {danio1}')
+etiqueta2.pack()
+etiqueta3 = tkinter.Label(f4, text = f'VIDA: {vida1}')
+etiqueta3.pack()
+etiqueta4 = tkinter.Label(f4, text = f'VELOCIDAD: {velocidad1}')
+etiqueta4.pack()
 
 ventana.mainloop()
