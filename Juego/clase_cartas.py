673d67db9a74fed18e736fa222fd88290d7dd950
Abigail2006 <abalbuena@sagradoalcal.edu.ar>
2022-08-09 17:20:31 -0300

    9 de agosto: organizamos un poco el código y agregamos algunas clases y funciones del juego.

--- Juego/clase_cartas.py
+++ Juego/clase_cartas.py
@@ -1,28 +1,14 @@
 from bd_utils import traer_carta_random
 from urllib.request import Request, urlopen
 from PIL import ImageTk, Image
+import pruebitauwu88
 
 class Jugador:
-    carta = {'vida': 0, 'danio': 0, 'foto': ''}
-    
-    def preparar_carta(self):
-        datos = traer_carta_random()
-        self.carta['vida'] = datos[1]
-        self.carta['danio'] = datos[2]
-
-        url_final = datos[0]
-        vida = datos[1]
-        danio = datos[2]
-        req = Request(datos[0], headers={'User-Agent': 'Mozilla/5.0'})
-        raw_data = urlopen(req).read()
-        #self.carta['foto'] = ImageTk.PhotoImage(data=raw_data)
-    
-    def golpear(self, oponente):
-        if oponente.carta['vida'] > 0:
-            oponente.carta['vida'] -= self.carta['danio']
-        else:
-            print("El Jugador no tiene vida")
-        return 'vida'
-    
     def __init__(self):
-        self.preparar_carta()
+        
+    def comp_vida(self, oponente):
+        if vida1 > vida2:
+            print("hola")
+        else:
+            print("chau")
+    
--- Juego/pruebitauwu88.py
+++ Juego/pruebitauwu88.py
@@ -3,74 +3,101 @@ from PIL import ImageTk, Image
 from bd_utils import traer_url_foto_por_id, traer_carta_random
 from urllib.request import Request, urlopen
 import login
+from functools import partial
 
-l = login.Login()
+# l = login.Login()
 
+class Pantalla:
+    def __init__(self):
+        ventana = tkinter.Tk()
+        ventana.title("Battle cards Roluby<3")
+        f1 = tkinter.Frame(ventana)
+        f1.grid(column=0, row=0)
+        f2 = tkinter.Frame(ventana)
+        f2.grid(column=1, row=0)
+        f3 = tkinter.Frame(ventana)
+        f3.grid(column=2, row=0)
+        f4 = tkinter.Frame(ventana)
+        f4.grid(column=0, row=1)
+        req = Request(j1.url_final, headers={'User-Agent': 'Mozilla/5.0'})
+        raw_data = urlopen(req).read()
+        photo = ImageTk.PhotoImage(data=raw_data)
+        l1 = tkinter.Label(f1, image=photo)
+        l1.grid(column=0, row=0)
+        lomo = ImageTk.PhotoImage(Image.open('lomo_clash.png'))
+        l2 = tkinter.Label(f3, image=lomo)
+        l2.grid(column=0, row=0)
 
-ventana = tkinter.Tk()
-ventana.title("Battle cards Roluby<3")
+        boton1 = tkinter.Button(f2, text = "Vida", command = partial(j1.comp_vida, j2))
+        boton1.pack()
+        boton2 = tkinter.Button(f2, text = "Daño", command = partial(j1.comp_danio, j2))
+        boton2.pack()
+        boton3 = tkinter.Button(f2, text = "Velocidad", command = partial(j1.comp_velocidad, j2))
+        boton3.pack()
+        boton4 = tkinter.Button(f2, text = "Rendirse", command = j1.rendirse)
+        boton4.pack()
 
-def preparar_carta():
-    datos = traer_carta_random()
-    url_final = datos[0]
-    vida = datos[1]
-    danio = datos[2]
-    velocidad = datos[3]
-    nombre = datos[4]
-    req = Request(url_final, headers={'User-Agent': 'Mozilla/5.0'})
-    raw_data = urlopen(req).read()
-    photo = ImageTk.PhotoImage(data=raw_data)
-    print(f'NOMBRE: {nombre} DAÑO: {danio} VIDA: {vida} VELOCIDAD: {velocidad}')
-    return (photo, vida, danio, velocidad, nombre)
+        etiqueta1 = tkinter.Label(f4, text = f'NOMBRE: {j1.nombre}')
+        etiqueta1.pack()
+        etiqueta2 = tkinter.Label(f4, text = f'DAÑO: {j1.danio}')
+        etiqueta2.pack()
+        etiqueta3 = tkinter.Label(f4, text = f'VIDA: {j1.vida}')
+        etiqueta3.pack()
+        etiqueta4 = tkinter.Label(f4, text = f'VELOCIDAD: {j1.velocidad}')
+        etiqueta4.pack()
+        etiqueta5 = tkinter.Label(f4, text = f'PUNTOS: {j1.puntos}')
+        etiqueta5.pack()
+        
+        ventana.mainloop()
 
+class Jugador:
+    def __init__(self):
+        self.puntos = 0
+        self.preparar_carta()
+        
+    def comp_vida(self, oponente):
+        if self.vida > oponente.vida:
+            self.puntos += 20
+            print("Ganó por: " + str(self.vida - oponente.vida))
+        else:
+            oponente.puntos += 20
+            print("Perdió por: " + str(oponente.vida - self.vida))
+    
+    def comp_danio(self, oponente):
+        if self.danio > oponente.danio:
+            self.puntos += 20
+            print("Ganó por: " + str(self.danio - oponente.danio))
+        else:
+            oponente.puntos += 20
+            print("Perdió por: " + str(oponente.danio - self.danio))
+            
+    def comp_velocidad(self, oponente):
+        if self.velocidad > oponente.velocidad:
+            self.puntos += 20
+            print("Ganó por: " + str(self.velocidad - oponente.velocidad))
+        else:
+            oponente.puntos += 20
+            print("Perdió por: " + str(oponente.velocidad - self.velocidad))
+            
+    def rendirse(self):
+        if True:
+            self.puntos -= 10
+            print("Se rindió. El juego empieza de nuevo")
+            self.preparar_carta
+            
+            
+    def preparar_carta(self):
+        datos = traer_carta_random()
+        self.url_final = datos[0]
+        self.vida = datos[1]
+        self.danio = datos[2]
+        self.velocidad = datos[3]
+        self.nombre = datos[4]
 
-f1 = tkinter.Frame(ventana)
-f1.grid(column=0, row=0)
-f2 = tkinter.Frame(ventana)
-f2.grid(column=1, row=0)
-f3 = tkinter.Frame(ventana)
-f3.grid(column=2, row=0)
-f4 = tkinter.Frame(ventana)
-f4.grid(column=0, row=1)
-
-carta1 = preparar_carta()
-ph1 = carta1[0]
-vida1 = carta1[1]
-danio1 = carta1[2]
-velocidad1 = carta1[3]
-nombre1 = carta1[4]
-
-
-
-l1 = tkinter.Label(f1, image=ph1)
-l1.grid(column=0, row=0)
-
-carta2 = preparar_carta()
-ph2 = carta2[0]
-
-
-
-
-lomo = ImageTk.PhotoImage(Image.open('lomo_clash.png'))
-l2 = tkinter.Label(f3, image=lomo)
-l2.grid(column=0, row=0)
-
-boton1 = tkinter.Button(f2, text = "Vida", command = lambda: print("La vida de j1 es _ y la de j2 es _"))
-boton1.pack()
-boton2 = tkinter.Button(f2, text = "Daño", command = lambda: print("El daño de j1 es _ y la de j2 es _"))
-boton2.pack()
-boton3 = tkinter.Button(f2, text = "Velocidad", command = lambda: print("La velocidad de j1 es _ y la de j2 es _"))
-boton3.pack()
-boton4 = tkinter.Button(f2, text = "Rendirse", command = lambda: print("_ se rindió"))
-boton4.pack()
-
-etiqueta1 = tkinter.Label(f4, text = f'NOMBRE: {nombre1}')
-etiqueta1.pack()
-etiqueta2 = tkinter.Label(f4, text = f'DAÑO: {danio1}')
-etiqueta2.pack()
-etiqueta3 = tkinter.Label(f4, text = f'VIDA: {vida1}')
-etiqueta3.pack()
-etiqueta4 = tkinter.Label(f4, text = f'VELOCIDAD: {velocidad1}')
-etiqueta4.pack()
-
-ventana.mainloop()
+if __name__ == "__main__":
+    
+    j1 = Jugador()
+    print(j1.vida)
+    j2 = Jugador()
+    print(j2.vida)
+    p = Pantalla()
