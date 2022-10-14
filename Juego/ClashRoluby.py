89f3a509cd849cf48f63a07bda925844dd94c6d8
abigail2006 <spianetti@sagradoalcal.edu.ar>
2022-10-14 11:09:32 -0300

    14 de octubre: actualización de la foto que se muestra en pantalla.

--- Juego/ClashRoluby.py
+++ Juego/ClashRoluby.py
@@ -1,76 +1,90 @@
 import tkinter
 from PIL import ImageTk, Image
-from bd_utils import traer_url_foto_por_id, traer_carta_random
+from bd_utils import traer_carta_random
 from urllib.request import Request, urlopen
 import login
 from functools import partial
-from itertools import cycle
+#from itertools import cycle
 
 l = login.Login()
 
 class Pantalla:
     def __init__(self):
         self.j1 = Jugador()
+        print(f'j1 vida = {self.j1.vida}')
+        
         self.j2 = Jugador()
-        ventana = tkinter.Tk()
-        ventana.title("Battle cards Roluby<3")
-        self.f1 = tkinter.Frame(ventana)
+        print(f'j2 vida = {self.j2.vida}')
+        self.ventana = tkinter.Tk()
+        self.ventana.title("Battle cards Roluby<3")
+        self.f1 = tkinter.Frame(self.ventana)
         self.f1.grid(column=0, row=0)
-        f2 = tkinter.Frame(ventana)
+        f2 = tkinter.Frame(self.ventana)
         f2.grid(column=1, row=0)
-        f3 = tkinter.Frame(ventana)
-        f3.grid(column=2, row=0)
-        f4 = tkinter.Frame(ventana)
+        self.f3 = tkinter.Frame(self.ventana)
+        self.f3.grid(column=2, row=0)
+        f4 = tkinter.Frame(self.ventana)
         f4.grid(column=0, row=1)
-        f5 = tkinter.Frame(ventana)
+        f5 = tkinter.Frame(self.ventana)
         f5.grid(column=2, row=1)
-        self.l1 = tkinter.Label(self.f1)
-        self.sacar_cartas()
         lomo = ImageTk.PhotoImage(Image.open('lomo_clash.png'))
-        self.l2 = tkinter.Label(f3, image=lomo)
+        self.photo = self.sacar_cartas(self.j1)
+        self.l1 = tkinter.Label(self.f1)
+        self.l1.grid(column=0, row=0)
+        self.l2 = tkinter.Label(self.f3, image=lomo)
         self.l2.grid(column=0, row=0)
-
+        self.poner_foto(self.l1,1)
+        
         boton1 = tkinter.Button(f2, text = "Vida", command = partial(self.jugar_mano, self.j1.comp_vida, self.j2))
         boton1.pack()
         boton2 = tkinter.Button(f2, text = "Daño", command = partial(self.jugar_mano, self.j1.comp_danio, self.j2))
         boton2.pack()
         boton3 = tkinter.Button(f2, text = "Velocidad", command = partial(self.jugar_mano, self.j1.comp_velocidad, self.j2))
         boton3.pack()
-        boton4 = tkinter.Button(f2, text = "Rendirse", command = partial(self.jugar_mano, self.j1.rendirse, None))
-        boton4.pack()
-
-        etiqueta1 = tkinter.Label(f4, text = f'NOMBRE: {self.j1.nombre}')
-        etiqueta1.pack()
-        etiqueta2 = tkinter.Label(f4, text = f'DAÑO: {self.j1.danio}')
-        etiqueta2.pack()
-        etiqueta3 = tkinter.Label(f4, text = f'VIDA: {self.j1.vida}')
-        etiqueta3.pack()
-        etiqueta4 = tkinter.Label(f4, text = f'VELOCIDAD: {self.j1.velocidad}')
-        etiqueta4.pack()
+        self.boton4 = tkinter.Button(f2, text = "Rendirse", command = partial(self.jugar_mano, self.j1.rendirse, None))
+        self.boton4.pack()
+        self.etiqueta1 = tkinter.Label(f4, text = f'NOMBRE: {self.j1.nombre}')
+        self.etiqueta1.pack()
+        self.etiqueta2 = tkinter.Label(f4, text = f'DAÑO: {self.j1.danio}')
+        self.etiqueta2.pack()
+        self.etiqueta3 = tkinter.Label(f4, text = f'VIDA: {self.j1.vida}')
+        self.etiqueta3.pack()
+        self.etiqueta4 = tkinter.Label(f4, text = f'VELOCIDAD: {self.j1.velocidad}')
+        self.etiqueta4.pack()
         self.etiqueta5 = tkinter.Label(f4, text = f'PUNTOS: {self.j1.puntos}')
         self.etiqueta5.pack()
         self.etiqueta6 = tkinter.Label(f5, text = "")
         self.etiqueta6.pack()
         
-        ventana.mainloop()
+        self.ventana.mainloop()
     
     def jugar_mano(self,funcion_jugador, atributo_jugada):
         respuesta = funcion_jugador(atributo_jugada)
+        self.etiqueta1.config(text = f'NOMBRE: {self.j1.nombre}')
+        self.etiqueta2.config(text = f'DAÑO: {self.j1.danio}')
+        self.etiqueta3.config(text = f'VIDA: {self.j1.vida}')
+        self.etiqueta4.config(text = f'VELOCIDAD: {self.j1.velocidad}')
         self.etiqueta5.config(text = f'PUNTOS: {self.j1.puntos}')
         self.etiqueta6.config(text = f'MENSAJE: {respuesta["mensaje"]}')
-        j1.preparar_carta()
-        j2.preparar_carta()
-        self.sacar_cartas()
+        self.j1.preparar_carta()
+        self.j2.preparar_carta()
+        print(self.j2.url_final)
+        self.boton4.config(state=tkinter.DISABLED)
+        self.photo = self.sacar_cartas(self.j1)
+        self.photo2 = self.sacar_cartas(self.j2)
+        self.poner_foto(self.l1,1)
     
-    def sacar_cartas(self):
-        req = Request(self.j1.url_final, headers={'User-Agent': 'Mozilla/5.0'})
+    def sacar_cartas(self, jugador):
+        req = Request(jugador.url_final, headers={'User-Agent': 'Mozilla/5.0'})
         raw_data = urlopen(req).read()
-        photo = ImageTk.PhotoImage(data=raw_data, master=self.l1)
-        self.l1.config(image=photo)
-        self.l1.grid(column=0, row=0)
+        p = ImageTk.PhotoImage(data=raw_data)
+        return p
         
-
-
+    def poner_foto(self, label, jug):
+        if jug == 1:
+            label.config(image=self.photo)
+        else:
+            label.config(image=self.photo2)
     
 class Jugador:
     def __init__(self):
@@ -93,20 +107,28 @@ class Jugador:
         return self.respuesta
     
     def comp_danio(self, oponente):
-        if self.danio > oponente.danio:
-            self.puntos += 20
+        if self.danio > oponente.danio:       
+            self.puntos += 20            
+            self.respuesta["mensaje"] = "Ganó por: " + str(self.danio - oponente.danio)
             print("Ganó por: " + str(self.danio - oponente.danio))
         else:
             oponente.puntos += 20
             print("Perdió por: " + str(oponente.danio - self.danio))
+            self.respuesta["mensaje"] = "Perdió por: " + str(oponente.danio - self.danio)
+        self.respuesta["puntos"] = self.puntos
+        return self.respuesta
             
     def comp_velocidad(self, oponente):
-        if self.velocidad > oponente.velocidad:
-            self.puntos += 20
+        if self.velocidad > oponente.velocidad:       
+            self.puntos += 20            
+            self.respuesta["mensaje"] = "Ganó por: " + str(self.velocidad - oponente.velocidad)
             print("Ganó por: " + str(self.velocidad - oponente.velocidad))
         else:
             oponente.puntos += 20
             print("Perdió por: " + str(oponente.velocidad - self.velocidad))
+            self.respuesta["mensaje"] = "Perdió por: " + str(oponente.velocidad - self.velocidad)
+        self.respuesta["puntos"] = self.puntos
+        return self.respuesta
             
     def rendirse(self):
         if True:
@@ -124,8 +146,13 @@ class Jugador:
         self.nombre = datos[4]
 
 if __name__ == "__main__":
-    j1 = Jugador()
-    print(j1.vida)
-    j2 = Jugador()
-    print(j2.vida)
     p = Pantalla()
+
+
+
+''' JSON -> diccionario
+datos = {
+"carta" = "....",
+"cartaOponente" = "....",
+"turno" = 1 o 2,
+}'''
